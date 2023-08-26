515835b10143d7b77bad10efaed72df233d384c1
root <lanremidelola@gmail.com>
2023-08-25 18:19:08 -0700

    alx

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 file { '/tmp/school':
   path    => '/tmp/school',
   content =>'I love Puppet',
-  owner   => 'www-data',
     mode    => '0744',
+   owner   => 'www-data',
   group   => 'www-data',
 }
