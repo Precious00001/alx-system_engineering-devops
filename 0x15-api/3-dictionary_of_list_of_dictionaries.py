cb15054efbb8e33a90e70c6125de48c72508bf3d
root <lanremidelola@gmail.com>
2023-09-26 03:01:17 +0000

    alx

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,45 +1,21 @@
 #!/usr/bin/python3
-"""
-Returns to-do list information for a given employee ID.
-"""
-
+"""Returns to-do list information for a given employee ID."""
 import json
 import requests
-from sys import argv
 
-
-if __name__ == "__main__":
-
-    import json
-    import requests
-    import sys
-
-    # Make an HTTP GET request to fetch a list of users
-    users_list = requests.get("https://jsonplaceholder.typicode.com/users")
-
-    # Convert the JSON response to a Python list of user objects
-    users = users_list.json()
-
-    # Make an HTTP GET request to fetch a list of TODOs
-    todo_list = requests.get('https://jsonplaceholder.typicode.com/todos')
-
-    # Convert the JSON response to a Python list of TODO objects
-    todos = todo_list.json()
-
-    # Create an empty dictionary to store TODO information for all employees
-    todoAll = {}
-
-# Loop through each user
-    for user in users:
-        taskList = []
-        for task in todos:
-            if task.get('userId') == user.get('id'):
-                taskDict = {"username": user.get('username'),
-                            "task": task.get('title'),
-                            "completed": task.get('completed')}
-                taskList.append(taskDict)
-        todoAll[user.get('id')] = taskList
-
-# Write the 'todoAll' dictionary to a JSON file
-    with open('todo_all_employees.json', mode='w') as f:
-        json.dump(todoAll, f)
+# Check if the script is being run as the main program
+if __name__ == '__main__':
+    # Define the output file where the JSON data will be stored
+    employees = "todo_all_employees.json"
+    # Make an HTTP GET request to fetch all to-do list items
+    users = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    # Make an HTTP GET request to fetch user information
+    users_id = requests.get('https://jsonplaceholder.typicode.com/users/').json()
+    # Open the output file for writing
+    with open(employees, "w") as f:
+        d = {j.get("id"): [{'task': i.get('title'),
+             'completed': i.get('completed'),
+                            'username': j.get('username')} for i in users
+                           if j.get("id") == i.get('userId')]
+             for j in users_id}
+        json.dump(d, f)
