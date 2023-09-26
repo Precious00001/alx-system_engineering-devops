ff5bdc31e865be63b0bd058e718a70ba2955df1e
root <lanremidelola@gmail.com>
2023-09-26 02:14:50 +0000

    alx

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,16 +11,18 @@ if __name__ == "__main__":
     id_c = sys.argv[1]
 
 # Define the base URL for the JSONPlaceholder API
-    url_user = "https://jsonplaceholder.typicode.com/"
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
 
 # Make an HTTP GET request to fetch user data based on the employee ID
-    user = requests.get(url_user + "users/{}".format(id_c)).json()
+    user = requests.get(url_user).json()
     username = user.get("username")
-    req = requests.get(url_user + "todos", params={"userId": id_c}).json()
+    req = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' +
+        (id_c) + '/todos')
 
 # Open a CSV file for writing, named after the employee ID
-    with open("{}.csv".format(id_c), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [id_c, username, t.get("completed"), t.get("title")]
-         ) for t in url_user]
+    with open("{}.csv".format(sys.argv[1]), "w") as file_c:
+        writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
+        for task in req.json():
+            writer.writerow([id_c, username,
+                            task.get("completed"), task.get("title")])
