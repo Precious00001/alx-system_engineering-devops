704d96f42b81a5678341b68b88b874150ce96482
root <lanremidelola@gmail.com>
2023-09-26 01:44:28 +0000

    alx

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,9 +11,11 @@ if __name__ == "__main__":
 
     # Make a GET request to fetch the user's information
     user = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(base_url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = requests.get(base_url + "todos",
+	params={"userId": sys.argv[1]}).json()
 
-    completed_tasks = [t.get("title") for t in todos if t.get("completed_tasks") is True]
+    completed_tasks = [t.get("title") for t in
+	todos if t.get("completed_tasks") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed_tasks), len(todos)))
     [print("\t {}".format(c)) for c in completed_tasks]
